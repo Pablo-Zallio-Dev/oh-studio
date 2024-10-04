58334e261b78c0b370655c4370bcfe9ca1d06b8f
Pablo Zallio Dev <pablozalliodev@gmail.com>
2024-10-04 16:10:58 +0200

    Finishing sections and affections

---
+++ src/profile.js
@@ -0,0 +1,66 @@
+ScrollReveal().reveal(".profile__title", {
+    duration: 1200,
+    origin: 'top',
+    distance: '-60px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+  ScrollReveal().reveal(".images__profiles__img", {
+    delay: 200,
+    duration: 1200,
+    origin: 'top',
+    distance: '-60px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+  ScrollReveal().reveal(".images__profiles__img--tablet", {
+    delay: 350,
+    duration: 1200,
+    origin: 'top',
+    distance: '-60px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+
+  ScrollReveal().reveal(".items--reveal", {
+    delay: 350,
+    duration: 1200,
+    origin: 'top',
+    distance: '-60px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+
+  ScrollReveal().reveal(".text--reveal", {
+    delay: 600,
+    duration: 1200,
+    origin: 'top',
+    distance: '-60px',
+    opacity: 0,
+    easing: 'ease'
+  });
+
+  ScrollReveal().reveal(".paragraph", {
+    delay: 250,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+
+  
+
+  ScrollReveal().reveal(".paragraph span", {
+    delay: 300,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
---
+++ src/script.js
@@ -0,0 +1,49 @@
+ScrollReveal().reveal(".main__text--text", {
+  duration: 1200,
+  origin: 'top',
+  distance: '-100px',
+  opacity: 0,
+  easing: 'ease'
+});
+
+
+ScrollReveal().reveal(".main__text--items", {
+    delay: 250,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+
+  ScrollReveal().reveal(".gallery__cards", {
+    delay: 250,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+
+  
+  ScrollReveal().reveal(".paragraph", {
+    delay: 250,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+
+  ScrollReveal().reveal(".paragraph span", {
+    delay: 300,
+    duration: 1200,
+    origin: 'top',
+    distance: '-100px',
+    opacity: 0,
+    easing: 'ease'
+  });
+  
+  
+  
